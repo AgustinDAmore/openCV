64b27897084e8dc9468a7dc11bb6e7d60e5849b4
Agustin <55986616+AgustinDAmore@users.noreply.github.com>
2022-11-08 18:15:17 -0300

    deteccionrostro

--- deteccionrostro.py
+++ deteccionrostro.py
@@ -1,18 +1,12 @@
 import cv2
 import os
 import imutils
-personName = 'Gaby'
-dataPath = 'C:/Users/Gaby/Desktop/Reconocimiento Facial/Data'#Cambia a la ruta donde hayas almacenado Data
-personPath = dataPath + '/' + personName
-if not os.path.exists(personPath):
-    print('Carpeta creada: ',personPath)
-    os.makedirs(personPath)
+
+
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture('Video.mp4')
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 count = 0
 while True:
-    
     ret, frame = cap.read()
     if ret == False: break
     frame =  imutils.resize(frame, width=640)
@@ -23,7 +17,6 @@ while True:
         cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
         rostro = auxFrame[y:y+h,x:x+w]
         rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
         count = count + 1
     cv2.imshow('frame',frame)
     k =  cv2.waitKey(1)
