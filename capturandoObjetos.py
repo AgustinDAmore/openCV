ea1648617584f308a2831902a42fa2bf766a65bd
AgustinDamore <agustindamore50@gmail.com>
2022-11-13 20:56:51 -0300

    mejoras en el recorte

--- capturandoObjetos.py
+++ capturandoObjetos.py
@@ -10,16 +10,28 @@ if not os.path.exists(Datos):
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-x1, y1 = 190, 80
-x2, y2 = 450, 398
+def nothing(x):
+    pass
 
-count = 0
+cv2.namedWindow('Imagen')
+cv2.createTrackbar('Y2',"Imagen",4,480,nothing)
+cv2.createTrackbar('X2',"Imagen",3,640,nothing)
+cv2.createTrackbar('X1',"Imagen",1,639,nothing)
+cv2.createTrackbar('Y1',"Imagen",2,479,nothing)
+
+count =79
 while True:
 
     ret, frame = cap.read()
     if ret == False: break
     imAux = frame.copy()
-    cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,0),2)
+
+    x2 = cv2.getTrackbarPos('X2',"Imagen")
+    y2 = cv2.getTrackbarPos('Y2',"Imagen")
+    x1 = cv2.getTrackbarPos('X1',"Imagen")
+    y1 = cv2.getTrackbarPos('Y1',"Imagen")
+    
+    cv2.rectangle(frame,(x1,y1),(x2,y2),(200,0,200),2)
 
     objeto = imAux[y1:y2,x1:x2]
     objeto = imutils.resize(objeto,width=38)
