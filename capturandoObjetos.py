63da67d7bea1e071907a607d60e92049c1d50d02
AgustinDamore <agustindamore50@gmail.com>
2022-11-13 21:17:41 -0300

    SELECTOR GUARDAR P O N

--- capturandoObjetos.py
+++ capturandoObjetos.py
@@ -3,6 +3,11 @@ import numpy as np
 import imutils
 import os
 
+Datos = 'p'
+if not os.path.exists(Datos):
+    print('Carpeta creada: ',Datos)
+    os.makedirs(Datos)
+
 Datos = 'n'
 if not os.path.exists(Datos):
     print('Carpeta creada: ',Datos)
@@ -18,8 +23,10 @@ cv2.createTrackbar('Y2',"Imagen",4,480,nothing)
 cv2.createTrackbar('X2',"Imagen",3,640,nothing)
 cv2.createTrackbar('X1',"Imagen",1,639,nothing)
 cv2.createTrackbar('Y1',"Imagen",2,479,nothing)
+cv2.createTrackbar('P o N',"Imagen",0,1,nothing)
 
-count =79
+countP = 1
+countN = 1
 while True:
 
     ret, frame = cap.read()
@@ -38,10 +45,19 @@ while True:
     #print(objeto.shape)
 
     k = cv2.waitKey(1)
-    if k == ord('s'):
-        cv2.imwrite(Datos+'/objeto_{}.jpg'.format(count),objeto)
-        print('Imagen guardada:'+'/objeto_{}.jpg'.format(count))
-        count = count +1
+    if k == ord('s') or k == ord('S'):
+        PoN = cv2.getTrackbarPos('P o N',"Imagen")
+        if PoN == 0:
+            Datos = 'p'
+            cv2.imwrite(Datos+'/objeto_{}_P.jpg'.format(countP),objeto)
+            print('Imagen guardada:'+'/objeto_{}_P.jpg'.format(countP))
+            countP += 1
+        else:
+            Datos = 'n'
+            cv2.imwrite(Datos+'/objeto_{}_N.jpg'.format(countN),objeto)
+            print('Imagen guardada:'+'/objeto_{}_N.jpg'.format(countN))
+            countN += 1
+
     if k == 27:
         break
 
